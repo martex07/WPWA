6a8fff6133077f615dbfcb7f2f24d5a147bf8b67
martex07 <golaszewska.07@gmail.com>
2021-09-21 18:28:12 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -5,31 +5,12 @@ document.getElementById('hamburger').addEventListener('click', navStatus);
 // Prüfen ob die Navigation geöffnet oder geschlossen ist
 
 function navStatus() {
+
     if (document.body.classList.contains('hamburger-active')) {
-        navClose();
+
+        document.body.classList.remove('hamburger-active');
     }
     else {
-        navOpen();
-    }
-}
-
-// Wenn die Navi geschlossen wird, Klasse für »offen« entfernen
-
-function navClose() {
-    document.body.classList.remove('hamburger-active');
-}
-
-// Wenn die Navi geöffnet wird, Klasse für »geschlossen« entfernen
-
-function navOpen() {
-    document.body.classList.add('hamburger-active');
-}
-
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-        x.style.display = "none";
-    } else {
-        x.style.display = "block";
+        document.body.classList.add('hamburger-active');
     }
 }
